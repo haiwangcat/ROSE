16793f88efd885dfa57fc7783703f18d04e7db45
Geoff Hulette <ghulette@gmail.com>
2011-04-17 15:11:05 -0700

    Tweaks

--- rote/keyvalue/main.cpp
+++ rote/keyvalue/main.cpp
@@ -2,10 +2,11 @@
 #include "Annotation.h"
 
 int main() {
+  string x = "ANNTYPE a1=50 a2=whatever a3=\"hello world\"";
   for(int i=0; i < 10; i++) {
-    Annotation *ann = Annotation::parse("ANNTYPE a1=50 a2=whatever a3=\"hello world\"");
+    Annotation *ann = Annotation::parse(x);
     if(ann != NULL) {
-      cout << "Id=" << ann->get_id() << endl;
+      cout << "id=" << ann->get_id() << endl;
       cout << "attrib a1=" << ann->get_attrib("a1")->int_value() << endl;
       cout << "attrib a2=" << ann->get_attrib("a2")->string_value() << endl;
       cout << "attrib a3=" << ann->get_attrib("a3")->string_value() << endl;
